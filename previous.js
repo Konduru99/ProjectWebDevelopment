7ef3a7c778591f4c9dd48c63e6c86f17b17de139
Konduru99 <konduru2999@gmail.com>
2025-08-20 13:03:19 -0700

    Add files via upload

---
+++ previous.js
@@ -0,0 +1,12 @@
+const unhideContent = (over, id) => {
+  let Buttons = document.querySelectorAll(".PrevBtn");
+  let MainContent = document.querySelectorAll(".MainContent");
+  let i = 0;
+  while (i < Buttons.length) {
+    Buttons[i].classList.remove("unhide");
+    MainContent[i].classList.remove("unhide");
+    i++;
+  }
+  over.classList.add("unhide");
+  document.getElementById(id).classList.add("unhide");
+};
