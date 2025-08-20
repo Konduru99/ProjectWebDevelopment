efa8501a30ad69b6bbb2e38043254f0329ae583b
Konduru99 <konduru2999@gmail.com>
2025-08-20 12:39:46 -0700

    Delete ProjectWebDevelopment-main directory

--- ProjectWebDevelopment-main/nav.js
+++
@@ -1,8 +0,0 @@
-const OpenClose = () => {
-  const navmenu = document.getElementById("menu");
-  if (navmenu.classList.contains("show")) {
-    navmenu.classList.remove("show");
-  } else {
-    navmenu.classList.add("show");
-  }
-}
